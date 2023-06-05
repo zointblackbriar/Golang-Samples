a158812bed170cb87eea7d62e5e3ca0e21bb01f9
zoint <zoint@gmail.com>
2023-06-05 19:27:52 +0200

    [NEWFEATURE] initial API Design with golang language

--- GoMicroserviceSamples/basic_http_server.go
+++ GoMicroserviceSamples/basic_http_server.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	//Zuweisen einen Portnummer
 	port := 8000
 	
-	http.HandleFunc("/sampleendpoint", sampleEndpoint)
-	log.Printf("Server is starting on port %v\n", port) 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+	// http.HandleFunc("/sampleendpoint", sampleEndpoint)
+	// log.Printf("Server is starting on port %v\n", port) 
+	// log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)) {
+		log.Prinf("Anfrage wurde angenommen")
+		w.Write([]byte("Hallo Welt"))
+	}
+
+	serverBeispiel := &http.Server {
+		Addr: fmt.Sprintf(":%d", port),
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1 << 20, //1 MB
+	}
+
+
 
 }
 
